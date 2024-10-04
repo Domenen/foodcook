4f6d82f2fd5533bc589cd256168a41977d1151ed
Sergey Beschstniy <Diman.9533@gmail.com>
2024-10-04 13:56:19 +0300

    try

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -14,19 +14,19 @@ class UserAdmin(admin.ModelAdmin):
 
 
 @admin.register(Subscription)
-class SubscriptionAdmin(admin.ModelAdmin):
+class SubscriptionAdmin(admin.BaseModelAdmin):
     list_display = ('pk', 'user', 'author')
     list_filter = ('user', 'author')
     search_fields = ('user', 'author')
 
-    def save_form(self, request, form, change):
-        if form.user == form.author:
-            form.author = None
-            return super().save_form(
-                self, request, form=None, change=change
-            )
-        return super().save_form(self, request, form, change)
-
 
 admin.site.unregister(Group)
 admin.site.unregister(TokenProxy)
+
+    # def save_form(self, request, form, change):
+    #     if form.user == form.author:
+    #         form.author = None
+    #         return super().save_form(
+    #             self, request, form=None, change=change
+    #         )
+    #     return super().save_form(self, request, form, change)
