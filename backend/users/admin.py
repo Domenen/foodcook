a9bda4fbcbbce649d8724d9e49aff026f2821371
Sergey Beschstniy <Diman.9533@gmail.com>
2024-10-04 14:26:42 +0300

    maybe

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.db import models
 from rest_framework.authtoken.models import TokenProxy
 
 from .models import User, Subscription
 
 
 @admin.register(User)
-class UserAdmin(admin.BaseModelAdmin):
+class UserAdmin(admin.ModelAdmin):
     list_display = ('pk', 'email', 'username', 'first_name',
                     'last_name', 'password')
     list_filter = ('username', 'email')
@@ -14,10 +15,22 @@ class UserAdmin(admin.BaseModelAdmin):
 
 
 @admin.register(Subscription)
-class SubscriptionAdmin(admin.BaseModelAdmin):
+class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('pk', 'user', 'author')
     list_filter = ('user', 'author')
     search_fields = ('user', 'author')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_user_author'
+            ),
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('author')),
+                name='prevent_self_subscription'
+            )
+        ]
 
 
 admin.site.unregister(Group)
