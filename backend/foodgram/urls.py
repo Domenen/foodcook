c60d361145a5e914e410bd02bb80c20ddbc75077
Sergey Beschstniy <Diman.9533@gmail.com>
2024-10-11 17:02:08 +0300

    try

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -7,5 +7,5 @@ from api.views import redirect_to_recipe
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-    path('s/<short_link>', redirect_to_recipe, name='redirect_full_url')
+    path('s/<short_url>', redirect_to_recipe, name='redirect_full_url')
 ]
