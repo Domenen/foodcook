bb3f25d6bc76da53a24658fbf72e9765b7b4fcc1
Sergey Beschstniy <Diman.9533@gmail.com>
2024-10-15 12:12:32 +0300

    try

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,8 +2,6 @@ from django.core.exceptions import ValidationError
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 from django.contrib.auth.models import Group
-from django.forms import ModelForm
-from django.http import HttpRequest
 from rest_framework.authtoken.models import TokenProxy
 
 from .models import User, Subscription
@@ -25,7 +23,9 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         if obj.user == obj.author:
-            raise ValidationError('Нельзя подписать пользователя на самого себя!')
+            raise ValidationError(
+                'Нельзя подписать пользователя на самого себя!'
+            )
         return super().save_model(request, obj, form, change)
 
 
