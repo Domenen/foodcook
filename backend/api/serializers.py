8c88d0b247996e40f6e6e2b281a216353df75fcd
Sergey Beschstniy <Diman.9533@gmail.com>
2024-10-17 14:58:09 +0300

    try

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -66,9 +66,7 @@ class UserSubscribeRepresentSerializer(UserGetSerializer):
 
     def get_recipes(self, obj):
         request = self.context.get['request']
-        recipes_limit = None
-        if request:
-            recipes_limit = request.query_params.get('recipes_limit')
+        recipes_limit = request.query_params.get('recipes_limit')
         recipes = obj.recipes.all()
         if recipes_limit:
             save_recipes_limit = int(recipes_limit)
