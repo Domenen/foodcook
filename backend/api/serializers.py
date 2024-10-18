8e6a2a7a2b10302d6c71740fc7d3b484491c8a31
Sergey Beschstniy <Diman.9533@gmail.com>
2024-10-18 17:29:34 +0300

    try

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -13,10 +13,10 @@ from .constants import (
 )
 
 
-class UserSignUpSerializer(UserCreateSerializer): 
-    class Meta: 
-        model = User 
-        fields = ('email', 'id', 'username', 'first_name', 
+class UserSignUpSerializer(UserCreateSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'id', 'username', 'first_name',
                   'last_name', 'password')
 
 
